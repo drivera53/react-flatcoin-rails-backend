f423940b05f6e7c0bc86e442056b7200a2a66183
Danny Rivera <samboard@hotmail.com>
2021-06-21 15:16:56 -0400

    Session controller added.

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,25 @@
+class SessionController < ApplicationController
+  def login
+    user = User.find_by :email=>params[:email]
+    if user && user.authenticate(params[:password])
+      payload = {user_id: user.id}
+      token = encode_token(payload)
+      user_json = user.to_json(:include => [
+        :portfolios=>{:include=> :trades}])
+      render json: {
+        user: user_json,
+        jwt: token
+      }
+    else 
+      render json: {status: "error", message: "We can't find a user according to your information, please try again."}
+    end
+  end
+
+  def auto_login
+    if session_user
+      render json: session_user, include: ['porfolio', 'portfolio.trades']
+    else
+      render json: {errors: "No User Logged In!"}
+    end
+  end
+end
